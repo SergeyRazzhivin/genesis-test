05f07dfc6b419d1fb8858f0a946b5a395239da67
SergeyRazjiwin <razgiva@gmail.com>
2024-10-13 15:03:48 +0300

    no message

--- back/src/app.controller.ts
+++ back/src/app.controller.ts
@@ -1,82 +1,94 @@
-import {Body, Controller, Get, Headers, Post, Req, Res} from '@nestjs/common';
-import {AppService} from './app.service';
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { AppService } from './app.service';
 import axios from 'axios';
-
 
 @Controller()
 export class AppController {
-    constructor(private readonly appService: AppService) {
-    }
+  constructor(private readonly appService: AppService) {}
 
-    @Get()
-    async auth(@Res() res) {
-        const clientId = '31992158';
-        const headers = {
-            'Content-Type': 'application/json',
-            'X-Client-Id': clientId,
-        };
-        try {
-            const {data} = await axios.get(`https://app2.gnzs.ru/amocrm/test/oauth/get-token.php?fil`, {headers});
-            res.cookie("access_token", data.access_token, {httpOnly: true});
-            // res.cookie("base_domain", data.base_domain, {httpOnly: true});
-            return res.json({})
-        } catch (error) {
-            throw error;
-        }
+  @Get()
+  async auth(@Res() res) {
+    const clientId = '31992158';
+    const headers = {
+      'Content-Type': 'application/json',
+      'X-Client-Id': clientId,
+    };
+    try {
+      const { data } = await axios.get(
+        `https://app2.gnzs.ru/amocrm/test/oauth/get-token.php?fil`,
+        { headers },
+      );
+      res.cookie('access_token', data.access_token, { httpOnly: true });
+      // res.cookie("base_domain", data.base_domain, {httpOnly: true});
+      return res.json({});
+    } catch (error) {
+      throw error;
     }
+  }
 
-    @Post('create-deal')
-    async createDeal(@Body() body,@Req() req) {
-        const token = req.cookies.access_token
-        const config = {
-            headers: {
-                Authorization: `Bearer ${token}`,
-                'X-Client-Id': 31992158,
-            },
-        };
-        try {
-            const response = await axios.post('https://amocrmgnzstesttask.amocrm.ru/api/v4/leads', body, config);
-            return response.data;
-        } catch (error) {
-            console.error('AmoCRM API error:', error.response?.data || error.message);
-            throw error;
-        }
+  @Post('create-deal')
+  async createDeal(@Body() body, @Req() req) {
+    const token = req.cookies.access_token;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'X-Client-Id': 31992158,
+      },
+    };
+    try {
+      const response = await axios.post(
+        'https://amocrmgnzstesttask.amocrm.ru/api/v4/leads',
+        body,
+        config,
+      );
+      return response.data;
+    } catch (error) {
+      console.error('AmoCRM API error:', error.response?.data || error.message);
+      throw error;
     }
+  }
 
-    @Post('create-contact')
-    async createContact(@Body() body, @Req() req) {
-        const token = req.cookies.access_token
-        const config = {
-            headers: {
-                Authorization: `Bearer ${token}`,
-                'X-Client-Id': 31992158,
-            },
-        };
-        try {
-            const response = await axios.post('https://amocrmgnzstesttask.amocrm.ru/api/v4/contacts', body, config);
-            console.log(response)
-            return response.data;
-        } catch (error) {
-            console.error('AmoCRM API error:', error.response?.data || error.message);
-            throw error;
-        }
+  @Post('create-contact')
+  async createContact(@Body() body, @Req() req) {
+    const token = req.cookies.access_token;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'X-Client-Id': 31992158,
+      },
+    };
+    try {
+      const response = await axios.post(
+        'https://amocrmgnzstesttask.amocrm.ru/api/v4/contacts',
+        body,
+        config,
+      );
+      return response.data;
+    } catch (error) {
+      console.error('AmoCRM API error:', error.response?.data || error.message);
+      throw error;
     }
+  }
 
-    @Post('create-company')
-    async createCompany(@Body() body, @Req() req) {
-        const token = req.cookies.access_token
-        const config = {
-            headers: {
-                Authorization: `Bearer ${token}`,
-                'X-Client-Id': 31992158,
-            },
-        };
-        try {
-            const response = await axios.post('https://amocrmgnzstesttask.amocrm.ru/api/v4/companies', body, config);
-            return response.data;
-        } catch (error) {
-            console.error('AmoCRM API error:', error.response?.data || error.message);
-            throw error;
-        }
+  @Post('create-company')
+  async createCompany(@Body() body, @Req() req) {
+    const token = req.cookies.access_token;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'X-Client-Id': 31992158,
+      },
+    };
+    try {
+      const response = await axios.post(
+        'https://amocrmgnzstesttask.amocrm.ru/api/v4/companies',
+        body,
+        config,
+      );
+      return response.data;
+    } catch (error) {
+      console.error('AmoCRM API error:', error.response?.data || error.message);
+      throw error;
     }
+  }
 }
