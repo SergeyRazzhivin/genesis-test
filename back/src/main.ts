691e899e130464918cc0509ae51b9023e0a0ed79
SergeyRazjiwin <razgiva@gmail.com>
2024-10-13 15:05:34 +0300

    no message

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,15 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {CorsOptions} from "@nestjs/common/interfaces/external/cors-options.interface";
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import * as cookieParser from 'cookie-parser';
 const corsConfig: CorsOptions = {
-  origin: ['http://localhost:5173'], // Добавьте все разрешенные источники
+  origin: ['http://localhost:5173'],
   credentials: true,
 };
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(corsConfig)
+  app.enableCors(corsConfig);
   app.use(cookieParser());
   await app.listen(3000);
 }
